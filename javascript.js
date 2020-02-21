45fa1c07c861741f641fe0449ac0937228bde4d3
mohan8692 <mohanmadaparthi123@gmail.com>
2020-02-21 22:01:52 +0530

    Mohan Madaparthi

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var a =[1001,1002,1003,1004,1005,1006,1007,1008,1009,10010];
+var b = a.length;
+
+/*StudentId();
+
+function StudentId(){
+	str=' ';
+	
+	for(var i=0;i < a.length;i++){
+		str+='<h3>'+a[i]+'</h3>';
+	}
+	str+='<h1>'+a.length+'</h1>';
+	
+	document.getElementById("datadisplay").innerHTML = str;
+	
+}*/
+document.getElementById("datadisplay").innerHTML = a;
+document.getElementById("length").innerHTML = b;
